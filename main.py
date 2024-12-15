98cfb915af401ff9c864eafb99fe05b6a1a58883
AlphastT101 <alphast.1001@gmail.com>
2024-12-15 23:16:49 +0600

    optimized something, but got messy. will do later.

--- main.py
+++ main.py
@@ -1,79 +1,70 @@
-import time
-start_time = time.time()
-
 import os
+import time
 import yaml
 import signal
 import asyncio
 import discord
+import uvicorn
 import requests
 from discord.ext import commands, tasks
 from pymongo.mongo_client import MongoClient
 
-# Slash commands import
 from slash.ai import ai_slash
 from slash.fun import fun_slash
 from slash.bot import bot_slash
-from slash.information import information_slash
 from slash.moderation import moderation_slash
+from slash.information import information_slash
 
-# Prefix commands import
 from prefix.ai import ai
-from prefix.bot import bbot
 from prefix.music import music
 
-# Events import
 from events.on_messages import on_messages
 from events.member_join import member_join
 from events.on_cmd_error import on_cmd_error
 
-# API import
 from api import app
 from bot_utilities.start_util import *
 from bot_utilities.ai_utils import process_queue
 
 def run_api():
     port = int(os.environ.get("PORT", config["flask"]["port"]))
-    import uvicorn
     uvicorn.run("api:app", host='0.0.0.0', port=port, log_level="warning")
 
-async def run_flask_app_async():
+async def run_flask_app_async(asyncio):
     loop = asyncio.get_running_loop()
-    await loop.run_in_executor(None, run_api)  # This will run run_flask_app in a separate thread
+    await loop.run_in_executor(None, run_api)
 
 with open("config.yml", "r") as config_file: config = yaml.safe_load(config_file)
-mongodb = config["bot"]["mongodb"]
-client = MongoClient(mongodb)
-bot_token, api = start(client)
-# sp_id, sp_secret = spotify_token(client) Not used for now
-
-is_generating = {}
-# member_histories_msg = {} Not used for now
-flask_thread = None
 flask_task = None
+flask_thread = None
+mongodb = config["bot"]["mongodb"]
+activity = discord.Game(name="/help")
 intents = discord.Intents.all()
 intents.presences = False
-activity = discord.Game(name="/help")
+# sp_id, sp_secret = spotify_token(client) Not used for now
 bot = commands.AutoShardedBot(
     shard_count=2,
     command_prefix=config["bot"]["prefix"],
-    intents=intents, activity=activity,
+    intents=intents,
+    activity=activity,
     help_command=None,
     reconnect=False
 )
+bot.is_generating = {}
+bot.start_time = time.time()
+bot.db = MongoClient(mongodb)
+bot_token, api = start(bot.db)
 
-
-bbot(bot, start_time, client)
 music(bot)
-ai(bot, is_generating)
+ai(bot)
 
-fun_slash(bot, client)
-moderation_slash(bot, client)
-information_slash(bot, client)
-bot_slash(bot, start_time, client)
-ai_slash(bot, client, is_generating)
+fun_slash(bot, bot.db)
+moderation_slash(bot, bot.db)
+information_slash(bot, bot.db)
+bot_slash(bot, bot.start_time, bot.db)
+ai_slash(bot)
 
-on_messages(bot, client)
+on_messages(bot, bot.db)
 on_cmd_error(bot)
 member_join(bot)
 
@@ -101,6 +92,7 @@ async def on_ready():
 
     await bot.load_extension("prefix.moderation")
     await bot.load_extension("prefix.information")
+    await bot.load_extension("prefix.owner")
     await bot.load_extension("prefix.fun")
 
     sync_slash_cmd.start()
@@ -108,10 +100,10 @@ async def on_ready():
     asyncio.create_task(process_queue())
 
     global flask_task  # Refer to the global variable
-    flask_task = asyncio.create_task(run_flask_app_async())
+    flask_task = asyncio.create_task(run_flask_app_async(asyncio))
 
     print("API Engine has been started.") 
-    print(f"Booted in {time.time() - start_time}s")
+    print(f"Booted in {time.time() - bot.start_time}s")
 
     await asyncio.sleep(2)
     requests.get("http://localhost:6750/create-task")
