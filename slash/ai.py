d0091257e0ea02ffb961f66e1befb5adf60ff388
AlphastT101 <alphast.1001@gmail.com>
2024-12-16 23:39:02 +0600

    optimized imports

--- slash/ai.py
+++ slash/ai.py
@@ -1,113 +1,14 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from bot_utilities.ai_utils import *
-from bot_utilities.ai_utils import vision
-from bot_utilities.owner_utils import check_blist
 from bot_utilities.api_utils import check_user, insert_token, delete_token, get_api_stat
 
-def ai_slash(bot):
+class AiSlash(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    """
-    @bot.tree.command(name="activate", description="Activate AI responses in a channel.")
-    @commands.guild_only()
-    @app_commands.describe(channel="Select the channel you want to activate the AI in(optional).")
-    async def activate_slash(interaction: discord.Interaction, channel: discord.TextChannel = None):
-        if await check_blist(interaction, mongodb): return
-        await interaction.response.defer(ephemeral=False)
-        if channel is None: channel = interaction.channel
-        channel_id = channel.id
-
-        if interaction.user.guild_permissions.administrator or interaction.user.id == 1026388699203772477:
-            insert_result = await insertdb("ai-channels",channel_id, mongodb)
-            if insert_result == "success":
-                await interaction.followup.send(embed=Embed(description="Success, now I'll respond to **all messages** in that channel.", color=discord.Colour.green()))
-            elif insert_result == "already set":
-                await interaction.followup.send(embed=Embed(description=":x: **Error**, that channel is already activated.", colour=discord.Colour.red()))
-        else:
-            await interaction.followup.send(embed=Embed(description="**You don't have permission to use this comamnd.**", color=discord.Color.red()),)
-    
-    
-    @commands.guild_only()
-    @bot.tree.command(name="deactivate", description="Deactivate AI responses in a channel")
-    @app_commands.describe(channel="Select the channel you want to activate the AI in(optional).")
-    async def deactivate_slash(interaction: discord.Interaction, channel: discord.TextChannel = None):
-        if await check_blist(interaction, mongodb): return
-        await interaction.response.defer(ephemeral=False)
-        if channel is None: channel = interaction.channel
-        channel_id = channel.id
-
-        if interaction.user.guild_permissions.administrator or interaction.user.id == 1026388699203772477:
-            delete_result = await deletedb("ai-channels", channel_id, mongodb)
-
-            if delete_result == "success":
-                await interaction.followup.send(embed=Embed(description="**✅ Successfully **disabled** that channel.**", color=discord.Color.green()),)
-            elif delete_result == "not found":
-                await interaction.followup.send(embed=Embed(description="❌ **Error**, that channel isn't activated.", colour=discord.Colour.red()))
-        else:
-            await interaction.followup.send(embed=Embed(description="**❌ You don't have permission to use this comamnd.**", color=discord.Color.red()),)
-    """
-
-    @bot.tree.command(name="ask", description="Ask LuminaryAI a question!")
-    @commands.guild_only()
-    @app_commands.describe(prompt="The question you want to ask LuminaryAI")
-    async def ask(interaction: discord.Interaction, prompt: str):
-        if await check_blist(interaction, bot.db): return
-        await interaction.followup.send(embed=discord.Embed(description="**❌ Please @ me to ask.**"))
-
-        
-        """" Use shapes api for now
-        await interaction.response.defer(ephemeral=False)
-        if prompt is None:
-            await interaction.followup.send(embed=discord.Embed(description="**❌ Please include your prompt!**"))
-            return
-
-        member_id = str(interaction.user.id)
-        history = member_histories_msg.get(member_id, [])
-
-        answer_embed = discord.Embed(
-            title="LuminaryAI - Loading",
-            description="Please wait while I process your request.",
-            color=0x99ccff,
-        )
-        answer_embed.set_footer(text="This may take a few moments", icon_url=bot.user.avatar.url)
-        answer = await interaction.followup.send(embed=answer_embed)
-
-        generated_message, updated_history = await generate_response_slash(interaction, prompt, history)
-        member_histories_msg[member_id] = updated_history
-
-        answer_generated = discord.Embed(
-            title="LuminaryAI - Response",
-            description=generated_message,
-            color=0x99ccff,
-        )
-        answer_generated.set_footer(text="Thanks for using LuminaryAI!", icon_url=bot.user.avatar.url)
-        await answer.edit(embed=answer_generated)
-        """
-
-
-
-    @bot.tree.command(name="vision", description="Vision an image")
-    @commands.guild_only()
-    @app_commands.describe(message="Enter your message.")
-    @app_commands.describe(image_link="Enter the image link.")
-    async def vision_command(interaction: discord.Interaction, message: str, image_link: str):
-        if await check_blist(interaction, bot.db): return
-        await interaction.response.defer(ephemeral=False)
-        response = await vision(message, image_link)
-
-        response_embed = discord.Embed(
-            title="LuminaryAI - vision",
-            description=response,
-            color=discord.Color.green() if response != "Ouch! Something went wrong!" else discord.Color.red()
-        )
-
-        response_embed.set_footer(text="Reply from LuminaryAI Image Vision. LuminaryAI does not guarantee the accuracy of the response provided. The Vision model is currently in **beta**")
-        await interaction.followup.send(embed=response_embed)
-
-
-    @bot.tree.command(name="imagine", description="Imagine an image using LuminaryAI")
-    @commands.guild_only()
+    @app_commands.command(name="imagine", description="Imagine an image using LuminaryAI")
+    @app_commands.guild_only()
     @app_commands.describe(
         prompt="Enter the prompt for the image to generate.",
         model="Select the model to use.",
@@ -126,25 +27,25 @@ def ai_slash(bot):
         app_commands.Choice(name="576x1024" ,value="576x1024"),
         app_commands.Choice(name="768x1024" ,value="768x1024")
     ])
-    async def imagine_pla(interaction: discord.Interaction, prompt: str, model: app_commands.Choice[str], size: app_commands.Choice[str]):
-        if await check_blist(interaction, bot.db): return
+    async def imagine_pla(self, interaction: discord.Interaction, prompt: str, model: app_commands.Choice[str], size: app_commands.Choice[str]):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=False)
 
         if model.value == "poli" and not size.value == "1024x1024":
             await interaction.followup.send(f"> **Size `{size.value}` is not available for polinations.ai**")
             return
 
-        if bot.is_generating.get(interaction.user.id):
+        if self.bot.is_generating.get(interaction.user.id):
             await interaction.followup.send("> **You're already generating an image!**")
             return
         if prompt is None:
             await interaction.followup.send("> **Please enter your prompt.**")
             return
 
-        bot.is_generating[interaction.user.id] = True
+        self.bot.is_generating[interaction.user.id] = True
         req = await interaction.followup.send("> **Please wait while I process your request.**")
 
-        link = await image_generate(model.value, prompt, size.value)
+        link = await self.bot.func_imagen(model.value, prompt, size.value, self.bot)
 
         try:
             embed = discord.Embed(
@@ -152,53 +53,55 @@ def ai_slash(bot):
                 description=f"Requested by: `{interaction.user}`\nPrompt: `{prompt}`",
                 color=discord.Color.blue()
             )
-            embed.set_footer(icon_url=bot.user.avatar.url, text="Thanks for using LuminaryAI!")
+            embed.set_footer(icon_url=self.bot.user.avatar.url, text="Thanks for using LuminaryAI!")
             embed.set_image(url=link)
             await req.edit(content="", embed=embed)
-            bot.is_generating[interaction.user.id] = False
+            self.bot.is_generating[interaction.user.id] = False
         except Exception as e:
             req.edit(content="> **❌ Ouch! something went wrong.**")
 
 
-    @bot.tree.command(name='poli', description="Generate images using Polinations.ai")
-    @commands.guild_only()
+    @app_commands.command(name='poli', description="Generate images using Polinations.ai")
+    @app_commands.guild_only()
     @app_commands.describe(prompt="Enter the prompt for the image to generate.")
-    async def poli_gen(interaction: discord.Interaction, prompt: str):
-        if await check_blist(interaction, bot.db): return
+    async def poli_gen(self, interaction: discord.Interaction, prompt: str):
+
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=False)
         delete_msg = await interaction.followup.send("> **⌚Please wait while I process your request.**")
-        async with aiohttp.ClientSession() as session:
+
+        async with self.bot.modules_aiohttp.ClientSession() as session:
+            image = await self.bot.func_poly_imgen(session, prompt, self.bot)
             send_embed = discord.Embed(
                 title="LuminaryAI - Image generation",
                 description=f"Requested by: `{interaction.user}`\nPrompt: `{prompt}`.",
                 color=0x99ccff,
             )
-            send_embed.set_footer(icon_url=bot.user.avatar.url, text="Thanks for using LuminaryAI!")
-            image = await poly_image_gen(session, prompt)
-            file=discord.File(image, 'generated_image.png')
+            send_embed.set_footer(icon_url=self.bot.user.avatar.url, text="Thanks for using LuminaryAI!")
             send_embed.set_image(url=f'attachment://generated_image.png')
             await delete_msg.delete()
-            await interaction.followup.send(content="", embed=send_embed, file=file)
+            await interaction.followup.send(content="", embed=send_embed, file=discord.File(image, 'generated_image.png'))
 
-    @bot.tree.command(name="search", description="Seaech the web for images and texts!")
-    @commands.guild_only()
+    @app_commands.command(name="search", description="Seaech the web for images and texts!")
+    @app_commands.guild_only()
     @app_commands.describe(prompt="Enter prompt for the web to search!")
-    async def search(interaction: discord.Interaction, prompt: str):
-        if await check_blist(interaction, bot.db): return
+    async def search(self, interaction: discord.Interaction, prompt: str):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=False)
-        image_urls = search_image(prompt)
-        await create_and_send_embed(prompt, bot, interaction, image_urls)
+        image_urls = self.bot.func_search_img(prompt, self.bot)
+        await self.bot.func_cse(discord, prompt, interaction, self.bot, image_urls)
 
 
-    @bot.tree.command(name="generate-api-key", description="Generate an API key for XET")
-    @commands.guild_only()
-    async def create_api(interaction: discord.Interaction):
-        if await check_blist(interaction, bot.db): return
+    @app_commands.command(name="generate-api-key", description="Generate an API key for XET")
+    @app_commands.guild_only()
+    async def create_api(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=True)
         
         if interaction.guild.id != 1144903052717985806:
             await interaction.followup.send("> :x: **You're not allowed to generate an API key in this server. Join XET to generate an API key: https://discord.com/invite/hmMBe8YyJ4**")
-            channel = interaction.guild.get_channel(1279262113503645706)
+            guild = await self.bot.fetch_guild(1144903052717985806)
+            channel = await guild.fetch_channel(1279262113503645706)
             await channel.send(f"{interaction.user.mention} | {interaction.user.id} Failed to generate an API key in {interaction.guild}")
             return
 
@@ -209,29 +112,27 @@ def ai_slash(bot):
             await channel.send(f"{interaction.user.mention} | {interaction.user.id} Failed to generate an API key in {interaction.guild} as the user is not verified")
             return
         
-        check = await check_user(bot.db, interaction.user.id)
-        
+        check = await check_user(self.bot.db, interaction.user.id)
         if check:
             await interaction.followup.send("> ❌ **You already have an API token! You can generate only one API token.**")
             return
         
-        token = await insert_token(bot.db, interaction.user.id)
+        token = await insert_token(self.bot.db, interaction.user.id)
         await interaction.followup.send(f"> ✅ **API token is generated successfully. Do not share this key with anyone, even if they are from LuminaryAI!**\n* **Join our support server for help on how to use the API Key.**\n* **API token:**\n```bash\n{token}```\n")
         channel = interaction.guild.get_channel(1279262113503645706)
         await channel.send(f"{interaction.user.mention} Generated an API key in <#{interaction.channel.id}>.")
 
-
-
-    @bot.tree.command(name="delete-api-key", description="Delete Your API key for XET")
-    @commands.guild_only()
-    async def delete_api(interaction: discord.Interaction):
-        if await check_blist(interaction, bot.db): return
+    @app_commands.command(name="delete-api-key", description="Delete Your API key for XET")
+    @app_commands.guild_only()
+    async def delete_api(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=False)
-        check = await check_user(bot.db, interaction.user.id)
+        check = await check_user(self.bot.db, interaction.user.id)
 
         if interaction.guild.id != 1144903052717985806:
             await interaction.followup.send("> :x: **You're not allowed to delete API key in this server. Join XET to generate an API key:  https://discord.com/invite/hmMBe8YyJ4**")
-            channel = interaction.guild.get_channel(1279262113503645706)
+            guild = await self.bot.fetch_guild(1144903052717985806)
+            channel = await guild.fetch_channel(1279262113503645706)
             await channel.send(f"{interaction.user.mention} | {interaction.user.id} Failed to delete API key in {interaction.guild}")
             return
         
@@ -242,7 +143,7 @@ def ai_slash(bot):
             await channel.send(f"{interaction.user.mention} | {interaction.user.id} Failed to delete API key in {interaction.guild} as the user is not verified.")
             return
         if check:
-            deleted = await delete_token(bot.db, interaction.user.id)
+            deleted = await delete_token(self.bot.db, interaction.user.id)
             if deleted:
                 await interaction.followup.send("> ✅ **API token is deleted successfully**")
                 channel = interaction.guild.get_channel(1279262113503645706)
@@ -252,12 +153,12 @@ def ai_slash(bot):
         else: await interaction.followup.send("> ❌ **You don't have an API key!**")
 
 
-    @bot.tree.command(name="api-stats", description="View our API stats")
-    @commands.guild_only()
-    async def delete_api(interaction: discord.Interaction):
-        if await check_blist(interaction, bot.db): return
+    @app_commands.command(name="api-stats", description="View our API stats")
+    @app_commands.guild_only()
+    async def delete_api(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=False)
-        stats = await get_api_stat(bot.db)
+        stats = await get_api_stat(self.bot.db)
 
         embed = discord.Embed(
             title="XET API Stats",
@@ -266,3 +167,6 @@ def ai_slash(bot):
         )
         embed.set_footer(text="Current XET API Stats")
         await interaction.followup.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(AiSlash(bot))
--- slash/information.py
+++ slash/information.py
@@ -1,22 +1,19 @@
-import discord
-from discord import app_commands
 from discord.ext import commands
-from bot_utilities.owner_utils import *
+from bot_utilities.help_embed import *
+from discord import app_commands, Interaction
 
+class InformationSlash(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-def information_slash(bot, mongodb):
-
-    @bot.tree.command(name='userinfo', description="View information about a user")
-    @commands.guild_only()
+    @app_commands.command(name='userinfo', description="View information about a user")
+    @app_commands.guild_only()
     @app_commands.describe(user="Select a user.")
-    async def user(interaction: discord.Interaction, user: discord.Member = None):
-
-        if await check_blist(interaction, mongodb): return
+    async def user(self, interaction: Interaction, user: discord.Member = None):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.defer(ephemeral=False)
 
-        if user is None:
-            user = interaction.user
-
+        if user is None:user = interaction.user
         roles = [role.mention for role in user.roles if role != interaction.guild.default_role]
         roles_string = ", ".join(roles) if roles else "No roles"
 
@@ -40,14 +37,129 @@ def information_slash(bot, mongodb):
         await interaction.followup.send(embed=embed)
 
 
-    @bot.tree.command(name='support', description="Shows support server invite link")
-    @commands.guild_only()
-    async def support(interaction: discord.Interaction):
-        if await check_blist(interaction, mongodb): return
+    @app_commands.command(name='support', description="Shows support server invite link")
+    @app_commands.guild_only()
+    async def support(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.send_message("> **Support server invite link:** [here](https://discord.com/invite/hmMBe8YyJ4)")
 
-    @bot.tree.command(name='owner', description="Shows bot owner")
-    @commands.guild_only()
-    async def owner(interaction: discord.Interaction):
-        if await check_blist(interaction, mongodb): return
+    @app_commands.command(name='owner', description="Shows bot owner")
+    @app_commands.guild_only()
+    async def owner(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
         await interaction.response.send_message("> **My owner is:** [AlphasT101](https://alphast101.netlify.app)")
+    
+    @app_commands.command(name="status", description="Check bot status")
+    @app_commands.guild_only()
+    async def check(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
+        await interaction.response.send_message("bot is online")
+
+    @app_commands.command(name="ping", description="See bot ping")
+    @app_commands.guild_only()
+    async def check(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
+        await interaction.response.defer(ephemeral=False)
+        latency_ms = round(self.bot.latency * 1000)
+        await interaction.followup.send(content=f'**Pong! My Latency is `{latency_ms}ms`.**')
+
+    @app_commands.command(name="uptime", description="Shows bot uptime")
+    @app_commands.guild_only()
+    async def uptime(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
+        await interaction.response.defer(ephemeral=False)
+        current_time = self.bot.modules_time.time()
+        difference = int(round(current_time - self.bot.start_time))
+        uptime_duration = self.bot.modules_datetime.timedelta(seconds=difference)
+
+        embed = discord.Embed(colour=0xc8dc6c)
+        embed.add_field(name="LuminaryAI - Uptime", value=str(uptime_duration))
+        await interaction.followup.send(embed=embed)
+
+    @app_commands.command(name="about", description="about the bot")
+    @app_commands.guild_only()
+    async def about(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
+        await interaction.response.defer(ephemeral=False)
+        about = await self.bot.about_embed(self.bot.start_time, self.bot)
+        owner = self.bot.get_user(1026388699203772477)
+        about.set_author(name="alphast101", icon_url=owner.avatar.url)
+        await interaction.followup.send(embed=about, file=discord.File("images/ai.png", filename="ai.png"))
+
+
+    @app_commands.command(name="help", description="Help/command list")
+    @app_commands.guild_only()
+    async def help(self, interaction: discord.Interaction):
+        if await self.bot.func_checkblist(interaction, self.bot.db): return
+        await interaction.response.defer(ephemeral=False)
+
+        help_view = self.bot.modules_view()
+        help_view.add_item(help_select)
+
+        help_embbed.set_thumbnail(url=self.bot.user.avatar)
+        help_msg = await interaction.followup.send(embed=help_embbed, view=help_view)
+        # Pagination buttons
+        buttons = [
+            self.bot.modules_button(label="Previous", style=discord.ButtonStyle.primary, custom_id='Previous'),
+            self.bot.modules_button(label="Next", style=discord.ButtonStyle.primary, custom_id='Next')
+        ]
+
+        help_view.add_item(buttons[0])
+        help_view.add_item(buttons[1])
+
+        # Variables to track current state
+        current_page = 0
+        current_commands = information_commannds
+        embed = embed_info
+
+        async def help_callback(interaction):
+            nonlocal current_page, current_commands, embed
+
+            if help_select.values[0] == "Information":
+                current_commands = information_commannds
+                embed = embed_info
+            elif help_select.values[0] == "AI":
+                current_commands = ai_commands
+                embed = embed_ai
+            elif help_select.values[0] == "Fun":
+                current_commands = fun_commands
+                embed = embed_fun
+            elif help_select.values[0] == "Moderation":
+                current_commands = moderation_commands
+                embed = embed_moderation
+            elif help_select.values[0] == "Automod":
+                current_commands = automod_commands
+                embed = embed_automod
+            elif help_select.values[0] == "Admin":
+                current_commands = admin_commands
+                embed = embed_admin
+            elif help_select.values[0] == "Music":
+                current_commands = music_commands
+                embed = embed_music
+
+            current_page = 0  # Reset to the first page
+            embed = get_chunk(embed, current_commands, current_page * 5)
+            await interaction.response.defer()
+            await help_msg.edit(embed=embed, view=help_view)
+
+        help_select.callback = help_callback
+
+        # Callback for the buttons
+        async def button_callback(interaction):
+            nonlocal current_page, current_commands, embed
+
+
+            if interaction.data["custom_id"] == "Previous":
+                current_page = max(current_page - 1, 0)
+            elif interaction.data["custom_id"] == "Next":
+                current_page = min(current_page + 1, (len(current_commands) - 1) // 5)
+
+            embed = get_chunk(embed, current_commands, current_page * 5)
+            await interaction.response.defer()
+            await help_msg.edit(embed=embed, view=help_view)
+
+        for button in buttons:
+            button.callback = button_callback
+
+async def setup(bot):
+    await bot.add_cog(InformationSlash(bot))
