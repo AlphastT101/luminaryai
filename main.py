9908a89df5a4da2ff38be620bbac9bc2ded746c7
AlphastT101 <alphast.1001@gmail.com>
2024-12-16 23:36:14 +0600

    use bot thungy to pass libraries

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
+import io
 import os
+import bs4
+import cv2
+import PIL
 import time
 import yaml
 import signal
+import random
+import aiohttp
 import asyncio
 import discord
 import uvicorn
+import datetime
 import requests
+import imagehash
+import numpy as np
+from openai import AsyncOpenAI
+from discord.ui import View, Button
 from discord.ext import commands, tasks
 from pymongo.mongo_client import MongoClient
 
-from slash.ai import ai_slash
 from slash.fun import fun_slash
-from slash.bot import bot_slash
 from slash.moderation import moderation_slash
-from slash.information import information_slash
-
-from prefix.ai import ai
-from prefix.music import music
 
 from events.on_messages import on_messages
 from events.member_join import member_join
 from events.on_cmd_error import on_cmd_error
 
 from api import app
-from bot_utilities.start_util import *
-from bot_utilities.ai_utils import process_queue
 
+from bot_utilities.start_util import *
+from bot_utilities.about_embed import about_embed
+from bot_utilities.owner_utils import check_blist
+from bot_utilities.ai_utils import image_generate, poly_image_gen, search_image, create_and_send_embed
+
+with open("config.yml", "r") as config_file: config = yaml.safe_load(config_file)
 def run_api():
     port = int(os.environ.get("PORT", config["flask"]["port"]))
     uvicorn.run("api:app", host='0.0.0.0', port=port, log_level="warning")
@@ -34,10 +43,6 @@ async def run_flask_app_async(asyncio):
     loop = asyncio.get_running_loop()
     await loop.run_in_executor(None, run_api)
 
-with open("config.yml", "r") as config_file: config = yaml.safe_load(config_file)
-flask_task = None
-flask_thread = None
-mongodb = config["bot"]["mongodb"]
 activity = discord.Game(name="/help")
 intents = discord.Intents.all()
 intents.presences = False
@@ -50,19 +55,47 @@ bot = commands.AutoShardedBot(
     help_command=None,
     reconnect=False
 )
-bot.is_generating = {}
-bot.start_time = time.time()
+flask_task = None
+flask_thread = None
+mongodb = config["bot"]["mongodb"]
 bot.db = MongoClient(mongodb)
-bot_token, api = start(bot.db)
+bot_token, openr_api_token = start(bot.db)
 
-music(bot)
-ai(bot)
+bot.modules_io = io
+bot.modules_np = np
+bot.modules_cv2 = cv2
+bot.modules_bs4 = bs4
+bot.modules_PIL = PIL
+bot.modules_time = time
+bot.modules_view = View
+bot.modules_random = random
+bot.modules_button = Button
+bot.modules_aiohttp = aiohttp
+bot.modules_datetime = datetime
+bot.modules_requests = requests
+bot.modules_imagehash = imagehash
+
+bot.about_embedd = about_embed
+bot.start_time = time.time()
+bot.is_generating = {}
+
+bot.func_imgen = image_generate
+bot.func_searchimg = search_image
+bot.func_checkblist = check_blist
+bot.func_poly_imgen = poly_image_gen
+bot.func_cse = create_and_send_embed
+
+bot.openai_client = AsyncOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=openr_api_token,
+)
+bot.xet_client = AsyncOpenAI(
+    base_url=f"http://localhost:{config["flask"]["port"]}/v1",
+    api_key="aner123!",
+)
 
 fun_slash(bot, bot.db)
 moderation_slash(bot, bot.db)
-information_slash(bot, bot.db)
-bot_slash(bot, bot.start_time, bot.db)
-ai_slash(bot)
 
 on_messages(bot, bot.db)
 on_cmd_error(bot)
@@ -92,14 +125,18 @@ async def on_ready():
 
     await bot.load_extension("prefix.moderation")
     await bot.load_extension("prefix.information")
+    await bot.load_extension("prefix.music")
     await bot.load_extension("prefix.owner")
     await bot.load_extension("prefix.fun")
+    await bot.load_extension("prefix.ai")
+
+    await bot.load_extension("slash.ai")
+    await bot.load_extension("slash.information")
 
     sync_slash_cmd.start()
     update_bio.start()
-    asyncio.create_task(process_queue())
 
-    global flask_task  # Refer to the global variable
+    global flask_task
     flask_task = asyncio.create_task(run_flask_app_async(asyncio))
 
     print("API Engine has been started.") 
@@ -122,7 +159,7 @@ async def on_guild_remove(guild):
 
 
 def handle_shutdown(signal, frame):
-    print("Shutdown signal received. Cleaning up...")
+    print("Shutdown signal received. Shutting down...")
     loop = asyncio.get_event_loop()
     loop.create_task(shutdown_bot())
 
