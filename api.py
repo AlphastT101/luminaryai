b7b8bd17e0e809372f3fd590bcb838b8dfa25f4c
AlphastT101 <alphast.1001@gmail.com>
2025-01-25 03:16:11 +0900

    improved

--- api.py
+++ api.py
@@ -24,18 +24,14 @@ from fastapi import FastAPI, Request, status, BackgroundTasks
 logger = logging.getLogger("custom_logger")
 logger.setLevel(logging.INFO)
 
-# File handler to log errors to a file
 file_handler = logging.FileHandler("error.log")
 file_handler.setLevel(logging.INFO)
-
-# Formatter for logging
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
 logger.addHandler(file_handler)
-
-# Suppress uvicorn's error logs
 logging.getLogger("uvicorn.error").propagate = False
+logging.getLogger("uvicorn.error").handlers = []
 
-# Suppress warnings
 warnings.filterwarnings("ignore", message="Using the in-memory storage for tracking rate limits")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -52,19 +48,21 @@ async def sync_api_stats():
     global api_stats
     while True:
         await save_api_stats(api_stats, clientdb)
-        api_stats = {}  # Resetting api_stats
+        api_stats = {}
         await asyncio.sleep(20)
 
-# Load configuration
-with open("config.yml", "r") as config_file:
+
+with open("config.yml", "r") as config_file: 
     config = yaml.safe_load(config_file)
 
 token_rate_limits = defaultdict(list)
-guild_id = str(config["api"]["guild_id"])
-guild_id_verify = str(config["api"]["guild_id_verify"])
+
+guild_id = str(config["api"]["api_guild_id"])
+guild_id_verify = str(config["api"]["verify_guild_id"])
+verified_role_id = str(config['api']['verified_role_id'])
 verification_email = config['api']['verification_email']
 
-ALGORITHM = "HS256" # JWT
+ALGORITHM = "HS256"
 clientdb = MongoClient(config["bot"]["mongodb"])
 clientdb['lumi-api']['accounts_registered'].create_index("expiresAt", expireAfterSeconds=0)
 clientdb['lumi-api']['jwt_tokens'].create_index("expiration", expireAfterSeconds=0)
@@ -79,7 +77,6 @@ sheader = {'Authorization': sbot}
 async def custom_internal_server_error_handler(request: Request, exc: Exception):
     logger.error(f"An error occurred: {exc}")
     return JSONResponse(status_code=500, content={"error": "Sorry, we've hit the ratelimit, we're working on it to fix it :( "})
-logging.getLogger("uvicorn.error").handlers = []
 
 @app.get('/')
 async def index():
@@ -87,7 +84,7 @@ async def index():
 
 @app.get("/create-task")
 async def create_task(request: Request):
-    if request.client.host != "127.0.0.1" and request.client.host != "::1":  # Allow only localhost (IPv4 and IPv6)
+    if request.client.host != "127.0.0.1" and request.client.host != "::1":
         return JSONResponse(status_code=403, content={"code": "403"})
     
     asyncio.create_task(sync_api_stats())
@@ -96,7 +93,7 @@ async def create_task(request: Request):
 
 @app.get("/shutdown")
 async def shutdown(request: Request):
-    if request.client.host != "127.0.0.1" and request.client.host != "::1":  # Allow only localhost (IPv4 and IPv6)
+    if request.client.host != "127.0.0.1" and request.client.host != "::1":
         return JSONResponse(status_code=403, content={"code": "403"})
     
     print("Shutting down the server...")
@@ -105,7 +102,6 @@ async def shutdown(request: Request):
 @app.post('/v1/images/generations')
 async def image(request: Request, background_tasks: BackgroundTasks):
 
-    # get token, prompt, model from the json
     try:
         token = request.headers.get('Authorization', '').split()[1] if 'Authorization' in request.headers else None
         if not token: return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"detail": "API token is not found in the JSON."})
@@ -125,10 +121,8 @@ async def image(request: Request, background_tasks: BackgroundTasks):
 
     api_stats[engine] = api_stats.get(engine, 0) + 1 # API Stats + 1
     result, discord_user, email = await check_token(clientdb, token)
-
     if not result: return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"detail": "Unauthorized, check your API token and try again."})
 
-    # Rate limiting by token
     current_time = time.time()
     if token.startswith("luminary") or token.startswith("XET"):
         token_rate_limits[token] = [timestamp for timestamp in token_rate_limits[token] if current_time - timestamp < 60]
@@ -144,10 +138,10 @@ async def image(request: Request, background_tasks: BackgroundTasks):
                 member_response = await client.get(f"https://discord.com/api/v10/guilds/{guild_id_verify}/members/{user_id}", headers=headers)
                 member_data = member_response.json()
                 user_roles = member_data.get('roles', [])
-                if "1279261339574861895" not in user_roles: # 1279261339574861895 is the role id, DO NOT CHANGE IT
+                if verified_role_id not in user_roles:
                     return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"detail":"Your account is not verified/You left the XET server."})
 
-    # Now, the user is verified, ratelimits are not exceed, token is valid. Continue to generate the image.
+
     characters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(characters) for _ in range(10))
 
@@ -191,7 +185,7 @@ async def image(request: Request, background_tasks: BackgroundTasks):
                             do_break = True
                             break
                         elif message['content'] != content and message_id not in checked:
-                            if xRetries >= 5: failed = True
+                            if xRetries >= 3: failed = True
                             await client.post(f'https://discord.com/api/v9/channels/{channel_id}/messages', json={"content": content}, headers=sheader)
                             xRetries+=1
 
@@ -236,7 +230,6 @@ async def text(request: Request):
     result, discord_user, email = await check_token(clientdb, token)
     if not result: return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"detail": "Invalid API token."})
 
-    # Rate limiting logic by token
     current_time = time.time()
     if token.startswith("luminary") or token.startswith("XET"):
         token_rate_limits[token] = [timestamp for timestamp in token_rate_limits[token] if current_time - timestamp < 60]
@@ -256,7 +249,6 @@ async def text(request: Request):
                 if "1279261339574861895" not in user_roles:
                     return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"detail": "Your account is not verified."})
 
-    # Now, the user is verified, ratelimits are not exceed, token is valid. Continue to text generation.
     response = await gen_text(open_r, messages, model)
     return JSONResponse(content={"choices": [{"message": {"content": response}}]})
 
@@ -398,7 +390,7 @@ async def list_tokens(request: Request, background_tasks: BackgroundTasks):
 
 
 if __name__ == '__main__':
-    # Suppress uvicorn's default access and error logging
+
     logging.getLogger("uvicorn.access").disabled = True
     logging.getLogger("uvicorn.error").disabled = True
     import uvicorn
@@ -406,5 +398,5 @@ if __name__ == '__main__':
         "api:app",
         host='0.0.0.0',
         port=int(os.environ.get("PORT", str(config["api"]["port"]))),
-        log_level="critical",  # Suppresses most of uvicorn's logs
+        log_level="critical",
     )
